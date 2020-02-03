9b1581a1afa615f886353b66484dd323bbb5a353
isane-hash <isanejossy@gmail.com>
2020-02-03 09:39:42 +0200

    first commit

---
+++ build/precache-manifest.274ca494b8cbbfc652d3376178f38c03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "647b51c6ada6ee2d8a2d7eb40b134003",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "89c2eefd8db8ea62de02",
+    "url": "/todolist/static/css/main.99257b17.chunk.css"
+  },
+  {
+    "revision": "27130051255a819fa7ea",
+    "url": "/todolist/static/js/2.7bd08d42.chunk.js"
+  },
+  {
+    "revision": "89c2eefd8db8ea62de02",
+    "url": "/todolist/static/js/main.06a37b37.chunk.js"
+  },
+  {
+    "revision": "5225356577f332e6df46",
+    "url": "/todolist/static/js/runtime~main.d3bf418b.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './App.css';
+import ListItems from './ListItems'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faTrash)
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.addItem = this.addItem.bind(this);
+    this.handleInput = this.handleInput.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text !==""){
+      const items = [...this.state.items, newItem];
+    this.setState({
+      items: items,
+      currentItem:{
+        text:'',
+        key:''
+      }
+    })
+    }
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  deleteItem(key){
+    const filteredItems= this.state.items.filter(item =>
+      item.key!==key);
+    this.setState({
+      items: filteredItems
+    })
+
+  }
+  
+ render(){
+  return (
+    <div className="App">
+      <header>
+        <h1 ><b>ToDoList</b></h1>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter Activity" value= {this.state.currentItem.text} onChange={this.handleInput}></input>
+          <button type="submit">Add</button>
+        </form>
+        <p>{this.state.items.text}</p>
+        
+          <ListItems items={this.state.items} deleteItem={this.deleteItem} />
+        
+      </header>
+    </div>
+  );
+ }
+}
+
+
+export default App;
